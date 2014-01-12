b6733254a06d5ae8e464c6c12c5409380cf4afc7
dangtanxp <dangtan_xp@yahoo.com.vn>
2014-01-12 19:49:00 +0700

    Update install.sh
    php 5.5.7

--- misc/install.sh
+++ misc/install.sh
@@ -66,9 +66,9 @@ make && make install
 cd ..
 
 echo "INSTALL PHP"
-wget http://de2.php.net/get/php-5.4.22.tar.gz/from/this/mirror
-tar -zxf php-5.4.22.tar.gz
-cd php-5.4.22
+wget http://de2.php.net/get/php-5.5.7.tar.gz/from/this/mirror
+tar -zxf php-5.5.7.tar.gz
+cd php-5.5.7
 ./configure \
 --prefix=$OPENSHIFT_RUNTIME_DIR/srv/php/ \
 --with-config-file-path=$OPENSHIFT_RUNTIME_DIR/srv/php/etc/apache2 \
